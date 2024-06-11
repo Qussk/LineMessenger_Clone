51c8d00335dd3cf7488b95b8091fc6ac69d793dd
Qussk <by1106007@gmail.com>
2024-06-11 16:32:36 +0900

    feat: add prj

---
+++ LineMessengerClone/LineMessengerClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LineMessengerClone
+//
+//  Created by 변윤나 on 6/11/24.
+//
+
+import SwiftUI
+  //test
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LineMessengerClone/LineMessengerClone/LineMessengerCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineMessengerCloneApp.swift
+//  LineMessengerClone
+//
+//  Created by 변윤나 on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LineMessengerCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

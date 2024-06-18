5039c806d59f960535f6d2cfa52d50e6a322ea33
Qussk <by1106007@gmail.com>
2024-06-18 12:42:50 +0900

    feat: add tabbar

---
+++ LineMessengerClone/LineMessengerClone/View/Home/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  LineMessengerClone
+//
+//  Created by 변윤나 on 6/18/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
--- LineMessengerClone/LineMessengerClone/View/MainTab/MainTabView.swift
+++ LineMessengerClone/LineMessengerClone/View/MainTab/MainTabView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct MainTabView: View {
+    @State private var selectedTab: MainTabType = .home
     var body: some View {
-        Text("MainTabView")
+        TabView(selection: $selectedTab) {
+            ForEach(MainTabType.allCases, id: \.self) { tab in
+                Group {
+                switch tab {
+                    case .home:
+                        HomeView()
+                    case .chat:
+                        ChatListView()
+                    case .phone:
+                        Color.bk2Fix1
+                    }
+                }
+                .tabItem {
+                    Label(tab.title, image: tab.imageName(selected: selectedTab == tab))
+                }
+                .tag(tab)
+            }
+        }
+        .tint(.bkText1)
     }
+    
+    init () {
+       // UITabBar.
+        UITabBar.appearance().unselectedItemTintColor = UIColor(Color.bkText1)
+    }
+
 }
 
 #Preview {
